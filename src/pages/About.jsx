757214f6c1c1fc846b0f64967ffde7d507f46480
humanbeing8118 <human.being1357900@gmail.com>
2026-04-02 15:00:53 -0700

    About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,34 @@
+import header from"../Header"
+import ProfileCard from "../Profile"
+import TextWithHeader from "../TextWithHeader"
+
 function About() {
  return (
  <>
-   <h1>About Page</h1>
- </>
-)
-};
+   <Header title="About us" subtitle="Meet The Team" />
 
+      <div className="grid grid-cols-2 gap-4">  
+        <ProfileCard 
+          cardEmoji="💉"
+          cardHeader="Alvin"
+          cardSubtitle="Goat"
+          cardBlurb="“Homelessness is not the result of not having a house, it’s a lack of a soul in a body.” – Goitsemang Mvula"
+        />
+        <div className="grid grid-cols-2 gap-4">  
+        <ProfileCard 
+          cardEmoji="💉"
+          cardHeader="Victor"
+          cardSubtitle="Not Goat"
+          cardBlurb="“Homeless exist because billionaires exist. Bridge the gap between the two and you will find balance.” – Deborah Bravandt"
+        />
+        </div>
+          <TextWithHeader
+          mySubtitle="Our Mission"
+          myText="Our mission is to spread awarness about how badly homelessness is and how fast it is spreading all around our community."
+        />
+      </div>
+</>
+
+)}
 
 export default About;
