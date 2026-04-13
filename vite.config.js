c0a7fbd4af495650fa32bef3e46ebc40f349961d
victorzhh <zhengvictor85@gmail.com>
2026-04-13 01:48:15 -0700

    s

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/final-project-victor-and-alvin/',
   plugins: [react(), tailwindcss()]
 })
