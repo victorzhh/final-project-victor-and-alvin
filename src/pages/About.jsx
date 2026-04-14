886f8dd2b42f49b21ac2ead1dcc986edcdad4141
victorzhh <zhengvictor85@gmail.com>
2026-04-13 21:00:41 -0700

    gurt

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,35 @@
-function About() {
- return (
- <>
-   <h1>About Page</h1>
- </>
-)
-};
+import Card from "../components/Card";
+import LikeButton from "../components/LikeButton";
 
+function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <div className="flex gap-6 justify-center flex-wrap p-6">
+        <Card
+          name="Victor"
+          title="Main Developer"
+          blurb="My name is Victor! I am a senior at Oakland Tech! I enjoy sleeping and playing video games! I hope you enjoy our website!"
+          github="https://github.com/victorzhh"
+          skills="HTML, Bootstrap, Tailwind"
+          quote="Homelessness is not the result of not having a house, it's a lack of a soul in a body. - Goitsemang Mvula"
+        >
+          <LikeButton />
+        </Card>
+
+        <Card
+          name="Alvin"
+          title="Developer"
+          blurb="My name is Alvin. When I play Valorant, Victor always carries me. I always rage and leave the game. Enjoy the website!"
+          github="https://github.com/humanbeing8118"
+          skills="Java, Python, Goofing Around"
+          quote="Homeless exist because billionaires exist. Bridge the gap between the two and you will find balance. - Deborah Bravandt"
+        >
+          <LikeButton />
+        </Card>
+      </div>
+    </>
+  );
+}
 
 export default About;
