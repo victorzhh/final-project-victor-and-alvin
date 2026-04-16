b30f419e4fda8f8c40cc5af17c1c3d197b9f25a1
victorzhh <zhengvictor85@gmail.com>
2026-04-16 09:59:16 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/my-first-app/>
       <Header />
        <Routes>
         <Route path="/" element={<Home />} />
