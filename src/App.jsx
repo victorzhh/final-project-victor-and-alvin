a24a29b63a2eef7ab047d5c47266f44d94761048
humanbeing8118 <human.being1357900@gmail.com>
2026-04-23 14:51:03 -0700

    undo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; // Adjust path if needed
-// Import other components as needed
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Database from "./pages/Database";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Default route */}
-        {/* Add other routes here, e.g., <Route path="/about" element={<About />} /> */}
+    <BrowserRouter>
+      <Header />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/database" element={<Database />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
